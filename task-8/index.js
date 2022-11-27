139c1a3d97e8e76dd5b1f60639c9cc6c137ef084
hks3333 <116485370+hks3333@users.noreply.github.com>
2022-11-27 11:56:53 +0530

    Drum Kit

---
+++ task-8/index.js
@@ -0,0 +1,42 @@
+function sound1(){
+    var aud=new Audio("sounds/crash.mp3");
+    aud.play();
+
+};
+
+
+function sound2(){
+    var aud=new Audio("sounds/kick-bass.mp3");
+    aud.play();
+
+};
+
+function sound3(){
+    var aud=new Audio("sounds/snare.mp3");
+    aud.play();
+
+};
+
+function sound4(){
+    var aud=new Audio("sounds/tom-1.mp3");
+    aud.play();
+
+};
+
+function sound5(){
+    var aud=new Audio("sounds/tom-2.mp3");
+    aud.play();
+
+};
+
+function sound6(){
+    var aud=new Audio("sounds/tom-3.mp3");
+    aud.play();
+
+};
+
+function sound7(){
+    var aud=new Audio("sounds/tom-4.mp3");
+    aud.play();
+
+};
